e79fe72d068e66e21273f3d668d0e431e0c84b25
Kiera Walton <kwalton@qa.com>
2021-02-05 15:19:25 +0000

    small changes to test

--- src/test/java/com/qa/todolist/rest/ToDoEntriesControllerUnitTest.java
+++ src/test/java/com/qa/todolist/rest/ToDoEntriesControllerUnitTest.java
@@ -37,14 +37,16 @@ public class ToDoEntriesControllerUnitTest {
 		// resources
 		Date date = Date.valueOf("2021-02-06");
 		ToDoEntriesDomain test_entry = new ToDoEntriesDomain(1L, "task 1", date, null);
-		ToDoEntriesDTO test_dto = this.mapper.map(test_entry, ToDoEntriesDTO.class);
-		//rules
+		ToDoEntriesDTO test_dto = mapToDTO(test_entry);
+		// rules
 		Mockito.when(this.service.readOne(1L)).thenReturn(test_dto);
-		//results
+		// results
 		ResponseEntity<ToDoEntriesDTO> result = this.controller.readOne(1L);
 		ResponseEntity<ToDoEntriesDTO> result2 = new ResponseEntity<ToDoEntriesDTO>(test_dto, HttpStatus.OK);
-		//assertions
-		Assertions.assertThat(result).isEqualTo(result2);	
+		// assertions
+		Assertions.assertThat(result).isEqualTo(result2);
+		
+		Mockito.verify(this.service, Mockito.times(1)).readOne(1L);
 	}
 
 	@Test
@@ -52,7 +54,7 @@ public class ToDoEntriesControllerUnitTest {
 		// resources
 		Date date = Date.valueOf("2021-02-06");
 		ToDoEntriesDomain test_entry = new ToDoEntriesDomain(1L, "task 1", date, null);
-		ToDoEntriesDTO test_dto = this.mapper.map(test_entry, ToDoEntriesDTO.class);
+		ToDoEntriesDTO test_dto = mapToDTO(test_entry);
 		// rules
 		Mockito.when(this.service.create(test_entry)).thenReturn(test_dto);
 		// results
@@ -62,15 +64,34 @@ public class ToDoEntriesControllerUnitTest {
 		Assertions.assertThat(result2).isEqualTo(result);
 
 		Mockito.verify(this.service, Mockito.times(1)).create(test_entry);
-
 	}
+
 	@Test
 	public void update() {
+		// resources
+		Date date = Date.valueOf("2021-02-06");
+		ToDoEntriesDomain test_entry = new ToDoEntriesDomain(1L, "task 1", date, null);
+		ToDoEntriesDomain updated_entry = new ToDoEntriesDomain(test_entry.getDescription(), test_entry.getDueDate(), test_entry.getMyList());
+		updated_entry.setId(1L);
+		ToDoEntriesDTO test_dto = mapToDTO(updated_entry);
+		// rules
+		Mockito.when(this.service.update(1L, updated_entry)).thenReturn(test_dto);
+		// results
+		ResponseEntity<ToDoEntriesDTO> result = this.controller.update(1L, updated_entry);
+		ResponseEntity<ToDoEntriesDTO> result2 = new ResponseEntity<ToDoEntriesDTO>(test_dto, HttpStatus.ACCEPTED);
+		//assertions
+		Assertions.assertThat(result).isEqualTo(result2);
 		
+		Mockito.verify(this.service, Mockito.times(1)).update(1L, updated_entry);
 	}
+
 	@Test
 	public void delete() {
-		
+//		//resources
+//		Date date = Date.valueOf("2021-02-06");
+//		ToDoEntriesDomain test_entry = new ToDoEntriesDomain(1L, "task 1", date, null);
+//		//rules
+//		Mockito.when(this.controller.delete(test_entry.getId())).then(null)
 	}
-	
+
 }
