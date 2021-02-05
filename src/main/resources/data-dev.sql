ea02b523711b1a3fc7c23bc4b1c0c705a6a3c97d
Kiera Walton <kwalton@qa.com>
2021-02-05 09:12:01 +0000

    populated data-dev.sql with some data

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,13 @@
+INSERT INTO TO_DO_LISTS_DOMAIN (TITLE)
+VALUES
+('General Tasks'),
+('Shopping List'),
+('QA Project 2 tasks');
+
+INSERT INTO TO_DO_ENTRIES_DOMAIN (DESCRIPTION,DUE_DATE,MY_LIST_ID)
+VALUES
+('create back end','2021-02-06',3),
+('testing for back end','2021-02-08',3),
+('create front end','2021-02-09',3),
+('buy dad a birthday present','2021-02-07',1),
+('send dbs check back to qa','2021-02-06',1);
