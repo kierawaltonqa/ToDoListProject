967ae44e8bfebfcaa43fe1ca7258025aafd23766
Kiera Walton <kwalton@qa.com>
2021-02-06 11:12:11 +0000

    domain list unit test coverage at 100%

--- src/test/java/com/qa/todolist/persistence/domain/ToDoListsDomainUnitTests.java
+++ src/test/java/com/qa/todolist/persistence/domain/ToDoListsDomainUnitTests.java
@@ -1,8 +1,14 @@
 package com.qa.todolist.persistence.domain;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.qa.todolist.persistence.dto.ToDoEntriesDTO;
 
 @SpringBootTest
 public class ToDoListsDomainUnitTests {
@@ -10,6 +16,10 @@ public class ToDoListsDomainUnitTests {
 	private ToDoListsDomain list = new ToDoListsDomain(1L,"list 1", null);
 	private ToDoListsDomain list2 = new ToDoListsDomain(2L,"list 2", null);
 	private ToDoListsDomain list3 = new ToDoListsDomain(2L,"list 2", null);
+	
+	private final List<ToDoEntriesDomain> entryList = new ArrayList<>();
+	private final Date date1 = Date.valueOf("2021-02-06");
+	private final ToDoEntriesDomain entry1 = new ToDoEntriesDomain(1L, "create back end", date1, null);
 	
 	@Test
 	public void settersTest() {
@@ -47,12 +57,13 @@ public class ToDoListsDomainUnitTests {
 	}
 	@Test
 	public void checkNonEquality() {
-		Assertions.assertThat(list).isNotEqualTo(list2);
+		Assertions.assertThat(list.getId()).isNotEqualTo(list2.getId());
+		Assertions.assertThat(list.getTitle()).isNotEqualTo(list2.getTitle());
 	}
 	@Test
 	public void oneListTitleNull() {
 		list2.setTitle(null);
-		Assertions.assertThat(list2).isNotEqualTo(list3);
+		Assertions.assertThat(list2.getTitle()).isNotEqualTo(list3.getTitle());
 	}
 	@Test
 	public void listTitlesNotEqual() {
@@ -72,6 +83,21 @@ public class ToDoListsDomainUnitTests {
 		list2.setId(null);
 		Assertions.assertThat(list.getId()).isNotEqualTo(list3.getId());
 	}
-
+	@Test
+	public void toDoListNotNull() {
+		entryList.add(entry1);
+		list2.setToDoList(entryList);
+		Assertions.assertThat(list2.getToDoList()).isNotNull();
+		Assertions.assertThat(list2.getToDoList()).isNotEqualTo(list3.getToDoList());
+	}
+	@Test
+	public void constructorWithoutId() {
+		ToDoListsDomain list4 = new ToDoListsDomain("List 4", null);
+		Assertions.assertThat(list4.getId()).isNull();
+		Assertions.assertThat(list4.getTitle()).isNotNull();
+		Assertions.assertThat(list4.getTitle()).isEqualTo("List 4");
+		Assertions.assertThat(list4.getToDoList()).isNull();
+	}
+	
 	
 }
