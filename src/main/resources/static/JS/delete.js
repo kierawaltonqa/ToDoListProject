59f6803dbff945b83ad88f579a5589ec83e6fad4
Kiera Walton <kwalton@qa.com>
2021-02-10 11:23:40 +0000

    delete list by list id method working

--- src/main/resources/static/JS/delete.js
+++ src/main/resources/static/JS/delete.js
@@ -1,2 +1,21 @@
 `use-strict`;
 
+const listid = document.querySelector("#deletelistId");
+
+const deleteButton = document.querySelector("#deleteListButton");
+
+//delete method for lists
+const deleteList = () => {
+    const id = listid.value;
+
+    fetch("http://localhost:8080/lists/delete/" + id, {
+        method: "DELETE",
+    })
+        // .then(response => response.json())
+        .then(response => console.log(`list with ID ${id} deleted`)
+        )
+        .catch(err => console.error(`Stop!! ${err}`));
+}
+
+//event listeners
+deleteButton.addEventListener("click", deleteList);
