d8c4b84a89cc3e35ad5094659c2619e1782e959a
Kiera Walton <kwalton@qa.com>
2021-02-10 11:53:13 +0000

    delete tast method working

--- src/main/resources/static/JS/delete.js
+++ src/main/resources/static/JS/delete.js
@@ -1,8 +1,10 @@
 `use-strict`;
 
 const listid = document.querySelector("#deletelistId");
+const taskid = document.querySelector("#deletetaskId");
 
 const deleteButton = document.querySelector("#deleteListButton");
+const deleteTaskButton = document.querySelector("#deleteTaskButton");
 
 //delete method for lists
 const deleteList = () => {
@@ -17,5 +19,16 @@ const deleteList = () => {
         .catch(err => console.error(`Stop!! ${err}`));
 }
 
+const deleteTask = () => {
+    const Id = taskid.value;
+
+    fetch("http://localhost:8080/entries/delete/" + Id, {
+        method: "DELETE",
+    }).then(response => console.log(`list with ID ${Id} deleted`)
+    )
+        .catch(err => console.error(`Stop!! ${err}`));
+}
+
 //event listeners
 deleteButton.addEventListener("click", deleteList);
+deleteTaskButton.addEventListener("click", deleteTask);
