e11dab3fa604561fb9ff8d42ef9943e4d9519faf
Kiera Walton <kwalton@qa.com>
2021-02-10 10:08:09 +0000

    button 'add' when creating new list - will try for adding tasks

--- src/main/resources/static/JS/createList.js
+++ src/main/resources/static/JS/createList.js
@@ -9,8 +9,14 @@ const showList = document.querySelector("#createList");
 const printScreen = (title) => {
     let element = document.createElement("h3");
     let text = document.createTextNode(`${title}`);
+    let button = document.createElement("button");
+    let button2 = document.createElement("button");
+    button.innerHTML = "Add Task";
+    button2.innerHTML = "Delete";
     element.appendChild(text);
     showList.appendChild(element);
+    showList.appendChild(button);
+    showList.appendChild(button2);
 }
 
 const createList = () => {
@@ -33,10 +39,26 @@ const createList = () => {
             console.log(info);
             //print title to screen
             printScreen(info.title);
-
         })
-        .catch(err => console.error(`Stopppppp! ${err}`));
+        .catch(err => console.error('ERROR!' + err));
 }
+
+const createTask = () => {
+    fetch("http://localhost:8080/entries/create", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+        .then(response => response.json())
+        .then(info => {
+            console.log(info);
+        })
+        .catch(err => console.error('ERROR!' + err));
+}
+
 
 createListButton.addEventListener("click", createList);
 
